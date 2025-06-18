b6f9f82774d4fae224d51756bb1a800894108aa7
hector <hserranodelafuente@gmail.com>
2025-06-18 21:53:57 +0200

    refactor viewsDeclaration

--- modules/views/viewsDeclaration.js
+++ modules/views/viewsDeclaration.js
@@ -1,17 +1,23 @@
 
-
-
-const ViewsRegister = [
+const views = [
     { html:'/public/views/',fileName:'basicLoginForm.html',serviceName:'/view/basic-login', serviceCore:'basicLogin' },
     { html:'/public/views/',fileName:'basicSignUpForm.html',serviceName:'/view/basic-signUp', serviceCore:'basicSignUp' },
     { html:'/public/views/',fileName:'init.html',serviceName:'/view/init', serviceCore:'init' },
     { html:'/public/views/',fileName:'startIndex.html',serviceName:'/view/basic-start', serviceCore:'basicStart' },
     { html:'/public/views/',fileName:'forgottenPasswordStepOne.html',serviceName:'/view/change-password-step-one', serviceCore:'forgottenPasswordStepOne' },
-    { html:'/public/views/',fileName:'forgottenPasswordStepTwo.html',serviceName:'/view/change-password-step-two', serviceCore:'forgottenPasswordStepTwo' },
-    
+    { html:'/public/views/',fileName:'forgottenPasswordStepTwo.html',serviceName:'/view/change-password-step-two', serviceCore:'forgottenPasswordStepTwo' }
+
+]
+
+const incrementalViews = [
     { html:'/public/incrementalViews/',fileName:'pageOne.html',serviceName:'/incrementalViews/pageOne', serviceCore:'basicStart' },
     { html:'/public/incrementalViews/',fileName:'pageTwo_One.html',serviceName:'/incrementalViews/pageTwo/pageTwo_One', serviceCore:'forgottenPasswordStepOne' },
     { html:'/public/incrementalViews/',fileName:'pageTwo_Two.html',serviceName:'/incrementalViews/pageTwo/pageTwo_Two', serviceCore:'forgottenPasswordStepTwo' }
+
 ]
+
+
+
+const ViewsRegister = [...views,...incrementalViews]
 
 module.exports = ViewsRegister
